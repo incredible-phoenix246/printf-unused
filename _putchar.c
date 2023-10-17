f98c0b3b900b4065042c80d1351ec953e49e6717
Ayomikun <ayomikuntemitope.araoye@gmail.com>
2023-10-17 14:32:38 -0700

    Glitch God pass you

--- _putchar.c
+++ _putchar.c
@@ -1,5 +1,5 @@
 #include "main.h"
-​
+
 /**
  * _putchar :uused for wrriting a single characher
  * @c: The character to print
@@ -11,7 +11,7 @@ int _putchar(char c)
 {
 	return (buffer(c));
 }
-​
+
 /**
  * buffer - Save the character in a buffer
  * @c: follow up buffer
@@ -22,15 +22,16 @@ int buffer(char c)
 {
 	static char buffering[1024];
 	static int i;
-	​
-		if (c == -1 || i == 1024)
-		{
-			write(1, buffering, i);
-			i = 0;
-		}
-	​
-		if (c != -1)
-			buffering[i++] = c;
-	​
-		return (1);
+
+	if (c == -1 || i == 1024)
+	{
+		write(1, buffering, i);
+		i = 0;
+	}
+
+	if (c != -1)
+		buffering[i++] = c;
+
+	return (1);
 }
+
