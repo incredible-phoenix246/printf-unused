a44597ed927201831be97d5e4b9a49644f464a86
harbiehorla <harbiehorla@gmail.com>
2023-10-17 14:25:19 -0700

    phoenix and wahala part 1

--- _putchar.c
+++ _putchar.c
@@ -1,17 +1,17 @@
 #include "main.h"
-​
 /**
- * _putchar :uused for wrriting a single characher
+ *_putchar: used for writing a single character
  * @c: The character to print
  *
  * Return: On success 1.
  * ony when sucessful
  */
+
 int _putchar(char c)
 {
 	return (buffer(c));
 }
-​
+
 /**
  * buffer - Save the character in a buffer
  * @c: follow up buffer
@@ -22,15 +22,12 @@ int buffer(char c)
 {
 	static char buffering[1024];
 	static int i;
-	​
 		if (c == -1 || i == 1024)
 		{
 			write(1, buffering, i);
 			i = 0;
 		}
-	​
 		if (c != -1)
 			buffering[i++] = c;
-	​
 		return (1);
 }
